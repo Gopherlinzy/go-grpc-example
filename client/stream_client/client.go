0c782fe27695ce66ad1584d2d12f16519cdb10dc
Gopherlinzy <2350621012@qq.com>
2022-11-03 12:40:10 +0800

    更新grpc stream示例

--- client/stream_client/client.go
+++ client/stream_client/client.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	pb "go-grpc-example/proto/stream"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"
 	"io"
 	"log"
+	"sync"
 	"time"
 )
 
 const PORT = "8888"
 
 func main() {
-	altsTC := alts.NewClientCreds(alts.DefaultClientOptions())
-	conn, err := grpc.Dial(":"+PORT, grpc.WithTransportCredentials(altsTC))
+	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("grpc.Dial err: %v", err)
 	}
@@ -22,15 +22,15 @@ func main() {
 
 	client := pb.NewStreamServiceClient(conn)
 
-	err = printLists(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: List", Value: 1234}})
-	if err != nil {
-		log.Fatalf("printLists.err: %v", err)
-	}
-
-	err = printRecord(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Record", Value: 9999}})
-	if err != nil {
-		log.Fatalf("printRecord.err: %v", err)
-	}
+	//err = printLists(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: List", Value: 1234}})
+	//if err != nil {
+	//	log.Fatalf("printLists.err: %v", err)
+	//}
+	//
+	//err = printRecord(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Record", Value: 9999}})
+	//if err != nil {
+	//	log.Fatalf("printRecord.err: %v", err)
+	//}
 
 	err = printRoute(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Route", Value: 1111}})
 	if err != nil {
@@ -38,14 +38,23 @@ func main() {
 	}
 }
 
+/*
+1. 建立连接 获取client
+2. 通过 client 获取stream
+3. for循环中通过stream.Recv()依次获取服务端推送的消息
+4. err==io.EOF则表示服务端关闭stream了
+*/
 func printLists(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	// 调用获取stream
 	stream, err := client.List(context.Background(), r)
 	if err != nil {
 		return err
 	}
-
+	// for循环获取服务端推送的消息
 	for {
+		// 通过 Recv() 不断获取服务端send()推送的消息
 		resp, err := stream.Recv()
+		// err==io.EOF则表示服务端关闭stream了
 		if err == io.EOF {
 			break
 		}
@@ -54,23 +63,31 @@ func printLists(client pb.StreamServiceClient, r *pb.StreamRequest) error {
 		}
 		log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
 	}
-
 	return nil
 }
 
+/*
+1. 建立连接并获取client
+2. 获取 stream 并通过 Send 方法不断推送数据到服务端
+3. 发送完成后通过stream.CloseAndRecv() 关闭steam并接收服务端返回结果
+*/
 func printRecord(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	// 获取 stream
 	stream, err := client.Record(context.Background())
 	if err != nil {
 		return err
 	}
 
 	for i := 0; i <= 6; i++ {
+		// 通过 Send 方法不断推送数据到服务端
 		err := stream.Send(r)
 		if err != nil {
 			return err
 		}
 	}
 
+	// 发送完成后通过stream.CloseAndRecv() 关闭steam并接收服务端返回结果
+	// (服务端则根据err==io.EOF来判断client是否关闭stream)
 	resp, err := stream.CloseAndRecv()
 	if err != nil {
 		return err
@@ -79,29 +96,59 @@ func printRecord(client pb.StreamServiceClient, r *pb.StreamRequest) error {
 	return nil
 }
 
+/*
+1. 建立连接 获取client
+2. 通过client获取stream
+3. 开两个goroutine 分别用于Recv()和Send()
+	3.1 一直Recv()到err==io.EOF(即服务端关闭stream)
+	3.2 Send()则由自己控制
+4. 发送完毕调用 stream.CloseSend()关闭stream 必须调用关闭 否则Server会一直尝试接收数据 一直报错...
+*/
 func printRoute(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	var wg sync.WaitGroup
+	// 调用方法获取stream
 	stream, err := client.Route(context.Background())
 	if err != nil {
 		return err
 	}
 
-	for n := 0; n <= 6; n++ {
-		err := stream.Send(r)
-		if err != nil {
-			return nil
+	// 开两个goroutine 分别用于Recv()和Send()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				fmt.Println("Server Closed")
+				break
+			}
+			if err != nil {
+				continue
+			}
+			log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for n := 0; n <= 6; n++ {
+			err := stream.Send(r)
+			if err != nil {
+				log.Printf("send error:%v\n", err)
+			}
+			time.Sleep(time.Second)
 		}
 
-		resp, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
+		// 发送完毕关闭stream
+		err = stream.CloseSend()
 		if err != nil {
-			return err
+			log.Printf("Send error:%v\n", err)
+			return
 		}
+	}()
 
-		log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
-		time.Sleep(time.Second)
-	}
-	stream.CloseSend()
+	wg.Wait()
 	return nil
 }
--- server/stream_server/server.go
+++ server/stream_server/server.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	pb "go-grpc-example/proto/stream"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"
 	"io"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -17,8 +18,7 @@ type StreamService struct {
 const PORT = "8888"
 
 func main() {
-	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
-	server := grpc.NewServer(grpc.Creds(altsTC)) //创建 gRPC Server 对象
+	server := grpc.NewServer() //创建 gRPC Server 对象
 	pb.RegisterStreamServiceServer(server, &StreamService{})
 
 	lis, err := net.Listen("tcp", ":"+PORT)
@@ -31,8 +31,16 @@ func main() {
 
 //服务端流式RPC，Server是Stream，Client为普通RPC请求
 //客户端发送一次普通的RPC请求，服务端通过流式响应多次发送数据集
+/*
+1. 建立连接 获取client
+2. 通过 client 获取stream
+3. for循环中通过stream.Recv()依次获取服务端推送的消息
+4. err==io.EOF则表示服务端关闭stream了
+*/
 func (s *StreamService) List(r *pb.StreamRequest, stream pb.StreamService_ListServer) error {
+	// 具体返回多少个response根据业务逻辑调整
 	for n := 0; n <= 6; n++ {
+		// 通过 send 方法不断推送数据
 		err := stream.Send(&pb.StreamResponse{
 			Pt: &pb.StreamPoint{
 				Name:  r.Pt.Name,
@@ -44,15 +52,26 @@ func (s *StreamService) List(r *pb.StreamRequest, stream pb.StreamService_ListSe
 		}
 		time.Sleep(time.Second)
 	}
+	// 返回nil表示已经完成响应
 	return nil
 }
 
 //客户端流式RPC，单向流
-//客户端通过流式多次发送RPC请求给服务端，服务端发送一次普通的RPC请求给客户端
+//客户端通过流式多次发送RPC请求给服务端，服务端发送一次响应给客户端
+/*
+1. for循环中通过stream.Recv()不断接收client传来的数据
+2. err == io.EOF表示客户端已经发送完毕关闭连接了,此时在等待服务端处理完并返回消息
+3. stream.SendAndClose() 发送消息并关闭连接(虽然在客户端流里服务器这边并不需要关闭 但是方法还是叫的这个名字，内部也只会调用Send())
+*/
 func (s *StreamService) Record(stream pb.StreamService_RecordServer) error {
+	// for循环接收客户端发送的消息
 	for {
+		// 通过 Recv() 不断获取客户端 send()推送的消息
 		r, err := stream.Recv()
+		// err == io.EOF表示已经获取全部数据
 		if err == io.EOF {
+			// SendAndClose 返回并关闭连接
+			// 在客户端发送完毕后服务端即可返回响应
 			return stream.SendAndClose(&pb.StreamResponse{Pt: &pb.StreamPoint{Name: "gRPC Stream Server: Record", Value: 1}})
 		}
 		if err != nil {
@@ -66,30 +85,57 @@ func (s *StreamService) Record(stream pb.StreamService_RecordServer) error {
 
 //双向流，由客户端发起流式的RPC方法请求，服务端以同样的流式RPC方法响应请求
 //首个请求一定是client发起，具体交互方法（谁先谁后，一次发多少，响应多少，什么时候关闭）根据程序编写方式来确定（可以结合协程）
+/*
+// 1. 建立连接 获取client
+// 2. 通过client调用方法获取stream
+// 3. 开两个goroutine（使用 chan 传递数据） 分别用于Recv()和Send()
+// 3.1 一直Recv()到err==io.EOF(即客户端关闭stream)
+// 3.2 Send()则自己控制什么时候Close 服务端stream没有close 只要跳出循环就算close了。 具体见https://github.com/grpc/grpc-go/issues/444
+*/
 func (s *StreamService) Route(stream pb.StreamService_RouteServer) error {
-	n := 0
-	for {
-		err := stream.Send(&pb.StreamResponse{
-			Pt: &pb.StreamPoint{
-				Name:  "gPRC Stream Client: Route",
-				Value: int32(n),
-			},
-		})
-		if err != nil {
-			return err
+	var (
+		wg    sync.WaitGroup //任务编排
+		msgCh = make(chan *pb.StreamPoint)
+	)
+	wg.Add(1)
+	go func() {
+		n := 0
+		defer wg.Done()
+		for v := range msgCh {
+			err := stream.Send(&pb.StreamResponse{
+				Pt: &pb.StreamPoint{
+					Name:  v.GetName(),
+					Value: int32(n),
+				},
+			})
+			if err != nil {
+				fmt.Println("Send error :", err)
+				continue
+			}
+			n++
 		}
+	}()
 
-		r, err := stream.Recv()
-		if err == io.EOF {
-			return nil
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			r, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("recv error :%v", err)
+			}
+			log.Printf("stream.Recv pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
+			msgCh <- &pb.StreamPoint{
+				Name: "gRPC Stream Server: Route",
+			}
 		}
-		if err != nil {
-			return err
-		}
+		close(msgCh)
+	}()
 
-		n++
+	wg.Wait() //等待任务结束
 
-		log.Printf("stream.Recv pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
-	}
 	return nil
 }
